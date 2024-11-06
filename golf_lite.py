dd94543f970442ec46b4cf3bb66f5255d11fee5b
chrcamp <cecampy@gmail.com>
2024-11-06 16:32:50 -0500

    Move GUI logic out of main.py. Create classes for GUI objects. Change Welcome Screen to Login.

--- golf_lite.py
+++ golf_lite.py
@@ -5,6 +5,8 @@ import golfer
 import holes
 import rounds
 import scores
+from customtkinter import *
+
 
 conn = sqlite3.connect(config.DB_NAME)
 c = conn.cursor()
@@ -19,3 +21,95 @@ def golf_lite_init_app():
     rounds.rounds_table_init()
     scores.scores_table_init()
     holes.holes_table_init()
+
+
+class Login(CTk):
+    def __init__(self):
+        super().__init__()
+        self.entry_password = None
+        self.entry_username = None
+        self.title('⛳️ Golf Lite - Login')
+        # self.overrideredirect(True)  # removes title bar
+        self.geometry('600x300+0+0')
+        self.create_widgets()
+
+    def create_widgets(self):
+        label_welcome = CTkLabel(self, text="Welcome to Golf Lite", font=("Avenir", 36), text_color='light green')
+        label_welcome.pack(side='top', pady=10)
+
+        label_username = CTkLabel(self, text='Username / Email')
+        label_username.pack()
+        self.entry_username = CTkEntry(self)
+        self.entry_username.pack(pady=(0, 10))
+
+        label_password = CTkLabel(self, text='Password')
+        label_password.pack()
+        self.entry_password = CTkEntry(self, show='*')
+        self.entry_password.pack(pady=(0, 20))
+
+        button_enter_app = CTkButton(self, text="Log in", command=self.verify_credentials)
+        button_enter_app.pack()
+
+        label_failed_login = CTkLabel(self, text='Login attempt failed.', text_color='red')
+        label_failed_login.pack()
+
+    def verify_credentials(self):
+        username = self.entry_username.get()
+        password = self.entry_password.get()
+        # TODO: Add logic
+        if True:
+            print(username, password)
+            self.open_main_app()
+
+    def open_main_app(self):
+        self.destroy()
+        app = App()
+        app.mainloop()
+
+
+class App(CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("Golf Lite App")
+        self.geometry('1200x700+0+0')
+        self.create_widgets()
+
+    def create_widgets(self):
+        for widget in self.winfo_children():
+            widget.destroy()
+
+        # Header
+        header = CTkFrame(self, fg_color="transparent")
+        header.pack(side='top', fill='x')
+        label_main = CTkLabel(header, text="Golf Lite", font=("Avenir", 24), text_color='light green')
+        label_main.pack(side='left', padx=10, pady=20)
+        exit_button = CTkButton(header, text="Quit App", command=self.destroy)
+        exit_button.pack(side='right', padx=10)
+
+        # Content
+        button_new_golfcourse = CTkButton(self, text="Add New Course", command=self.add_new_course_form)
+        button_new_golfcourse.pack(pady=80)
+
+    def add_new_course_form(self):
+        for widget in self.winfo_children():
+            widget.destroy()
+
+        # Header
+        header = CTkFrame(self, fg_color="transparent")
+        header.pack(side='top', fill='x')
+        label_new_course = CTkLabel(header, text="Add New Course", font=("Avenir", 18), text_color='light green')
+        label_new_course.pack(side='top', pady=20)
+
+        # Form Contents
+        input_course_name = CTkEntry(self, placeholder_text='Course Name')
+        input_course_name.pack()
+        input_course_location = CTkEntry(self, placeholder_text='Course Location')
+        input_course_location.pack()
+
+        # Footer
+        footer = CTkFrame(self, width=200, fg_color="transparent")
+        footer.pack(side='bottom')
+        button_cancel = CTkButton(footer, text="Cancel", command=self.create_widgets)
+        button_cancel.pack(side='left', padx=10, pady=20)
+        button_add_course = CTkButton(footer, text="Add Course")
+        button_add_course.pack(side='right', padx=10, pady=20)
--- main.py
+++ main.py
@@ -7,79 +7,15 @@ from customtkinter import *
 # conn = sqlite3.connect(':memory:')
 conn = sqlite3.connect(config.DB_NAME)
 c = conn.cursor()
-glf.golf_lite_init_app()
 
 # Set appearance and theme
 set_appearance_mode("dark")  # Modes: "system" (default), "dark", "light"
 set_default_color_theme("green")  # Themes: "blue" (standard), "green", "dark-blue"
 
-# Initialize Main Window
-window = CTk()
-window.title("Golf Lite App")
-window.geometry('1200x700+0+0')
 
+if __name__ == '__main__':
+    # Initialize the UI by showing the welcome screen
+    login = glf.Login()
+    login.mainloop()
 
-# Define welcome screen function
-def show_welcome_screen():
-    # Clear existing widgets
-    for widget in window.winfo_children():
-        widget.destroy()
-
-    # Welcome Label
-    label_welcome = CTkLabel(window, text="Welcome to Golf Lite", font=("Avenir", 36), text_color='light green')
-    label_welcome.pack(pady=80)
-
-    # Enter App Button
-    button_enter_app = CTkButton(window, text="Enter App", command=show_main_screen)
-    button_enter_app.pack()
-
-
-def show_main_screen():
-    # Clear existing widgets
-    for widget in window.winfo_children():
-        widget.destroy()
-
-    # Main Screen Contents
-    header = CTkFrame(window, fg_color="transparent")
-    header.pack(side='top', fill='x')
-
-    label_main = CTkLabel(header, text="Golf Lite", font=("Avenir", 24), text_color='light green')
-    label_main.pack(side='left', padx=10, pady=20)
-    exit_button = CTkButton(header, text="Quit App", command=window.destroy)
-    exit_button.pack(side='right', padx=10)
-
-    button_new_golfcourse = CTkButton(window, text="Add New Course", command=add_new_course_form)
-    button_new_golfcourse.pack(pady=80)
-
-
-def add_new_course_form():
-    for widget in window.winfo_children():
-        widget.destroy()
-
-    # Header
-    header = CTkFrame(window, fg_color="transparent")
-    header.pack(side='top', fill='x')
-    label_new_course = CTkLabel(header, text="Add New Course", font=("Avenir", 18), text_color='light green')
-    label_new_course.pack(side='top', pady=20)
-
-    # Form Contents
-    input_course_name = CTkEntry(window, placeholder_text='Course Name')
-    input_course_name.pack()
-    input_course_location = CTkEntry(window, placeholder_text='Course Location')
-    input_course_location.pack()
-
-    # Footer
-    footer = CTkFrame(window, width=200, fg_color="transparent")
-    footer.pack(side='bottom')
-    button_cancel = CTkButton(footer, text="Cancel", command=show_main_screen)
-    button_cancel.pack(side='left', padx=10, pady=20)
-    button_add_course = CTkButton(footer, text="Add Course")
-    button_add_course.pack(side='right', padx=10, pady=20)
-
-
-# Initialize the UI by showing the welcome screen
-show_welcome_screen()
-
-# Run the app
-window.mainloop()
-conn.close()
+    conn.close()
